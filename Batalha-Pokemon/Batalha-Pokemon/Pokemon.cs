d21c77d2856cf20fa61e19b4044b67bb29955569
Dyego Anjos <107328787+DyegoAnjos@users.noreply.github.com>
2023-05-28 23:42:22 -0300

    1.0

---
+++ Batalha-Pokemon/Batalha-Pokemon/Pokemon.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batalha_Pokemon
+{
+    internal class Pokemon
+    {
+        public int codPokemon = 0;
+        public string nomePokemon = "Defaut";
+        private int vidaPokemon = 1;
+        private int vidaMaxPokemon = 1;
+        private int danoPokemon = 1;
+        private int curaPokemon = 1;
+
+        public Pokemon(int cod, string n, int vmax, int d, int c) 
+        {
+            codPokemon=cod;
+            nomePokemon = n;
+            SetVidaMaxPokemon(vmax);
+            SetVidaPokemon(vmax);
+            SetDanoPokemon(d);
+            SetCuraPokemon(c);
+        }
+
+        
+
+        public bool SetVidaMaxPokemon(int vida)
+        {
+            if (vida > 0 && vida >= vidaPokemon)
+            {
+                vidaMaxPokemon = vida;
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public int GetVidaMaxPokemon()
+        {
+            return vidaMaxPokemon;
+        }
+
+
+        public bool SetVidaPokemon(int vida) {
+            if (vida <= vidaMaxPokemon && vida > 0)
+            {
+                vidaPokemon = vida;
+
+                return true;
+            }
+            
+            return false;
+        }
+
+        public int GetVidaPokemon()
+        {
+            return vidaPokemon;
+        }
+
+        public bool SetDanoPokemon(int dano)
+        {
+            if (dano > 0)
+            {
+                danoPokemon = dano;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetDanoPokemon()
+        {
+            return danoPokemon;
+        }
+        
+        public bool SetCuraPokemon(int cura)
+        {
+            if (cura > 0)
+            {
+                curaPokemon = cura;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetCuraPokemon()
+        {
+            return curaPokemon;
+        }
+
+        public int AtaquePokemon(int vidaInimigo)
+        {
+            return vidaInimigo - danoPokemon;
+        }
+
+        public bool CuraPokemon()
+        {
+            if(vidaPokemon <= 0)
+                return false;
+
+            if (curaPokemon + vidaPokemon >= vidaMaxPokemon)
+            {
+                vidaPokemon = vidaMaxPokemon;
+                
+                return true;
+            }
+
+            else
+            {
+                vidaPokemon += curaPokemon;
+                return true;
+            }
+
+        }
+
+    }
+}
---
+++ Batalha-Pokemon/Batalha-Pokemon/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batalha_Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon[] Pokebox = new Pokemon[]
+            {
+                new Pokemon(1,"Venusaur", 364, 60, 90),
+                new Pokemon(2,"Charizard", 380, 75, 20),
+                new Pokemon(3,"Blastoise", 500, 65, 20),
+            };
+
+            Pokemon PokemonAtual = new Pokemon(0,"Defult",0,0,0);
+
+            int opition;
+
+            Console.WriteLine("Olá futuro mestre pokémon:");
+
+            Console.WriteLine("1-Venusaur\n2-Charizard\n3-Blastoise");
+            opition = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i <= Pokebox.Length-1; i++)
+            {
+
+                if (opition == Pokebox[i].codPokemon)
+                {
+                    PokemonAtual = Pokebox[i];
+                    break;
+                }
+                    
+            }
+
+            
+
+         
+
+            
+            Console.ReadLine();
+        }
+    }
+}
